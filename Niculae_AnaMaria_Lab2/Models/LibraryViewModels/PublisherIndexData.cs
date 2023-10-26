9ab60c280c1760c64418d9a89872896572e1e11d
ana11090 <72621705+ana11090@users.noreply.github.com>
2023-10-26 17:30:59 +0300

    Terminat

--- Niculae_AnaMaria_Lab2/Models/LibraryViewModels/PublisherIndexData.cs
+++ Niculae_AnaMaria_Lab2/Models/LibraryViewModels/PublisherIndexData.cs
@@ -5,5 +5,7 @@
         public IEnumerable<Publisher> Publishers { get; set; }
         public IEnumerable<Book> Books { get; set; }
         public IEnumerable<Order> Orders { get; set; }
+
+        public IEnumerable<Customer> Customers { get; set; }
     }
 }
